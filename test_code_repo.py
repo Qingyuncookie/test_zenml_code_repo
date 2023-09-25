b1abde621665f9f4ba537debae2331a6ddb88cb9
Qingyuncookie <1456258898@qq.com>
2023-09-25 17:23:17 +0800

    test

--- test_code_repo.py
+++ test_code_repo.py
@@ -1,8 +1,9 @@
 from zenml import pipeline, step
 from zenml.config import DockerSettings
 
-docker_settings = DockerSettings(parent_image="zenmldocker/zenml:0.42.0-py3.10",
-                                 skip_build=True,
+docker_settings = DockerSettings(parent_image=\
+    "packages.glodon.com/docker-intellisense-snapshots/zenml:chatglm2_0915-orchestrator",
+    skip_build=True,
     source_files="download")
 
 @step(enable_cache=False)
