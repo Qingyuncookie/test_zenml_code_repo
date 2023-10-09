5d9995a482150e84da702bd80dea14d0f01c60e1
liqy-a <liqy-a@glodon.com>
2023-10-09 12:05:38 +0800

    confirm download code path

--- test_code_repo.py
+++ test_code_repo.py
@@ -1,8 +1,8 @@
 from zenml import pipeline, step
 from zenml.config import DockerSettings
 
-docker_settings = DockerSettings(parent_image="packages.glodon.com/docker-intellisense-snapshots/chatglm-zenml-gpu:glodon_v4",
-    skip_build=False,
+docker_settings = DockerSettings(parent_image="zenml:test_repo_ppl-orchestrator",
+    skip_build=True,
     source_files="download")
 
 def on_failure(exception: BaseException):
@@ -12,6 +12,9 @@ def on_failure(exception: BaseException):
 def test_step():
     print("ok")
     print("it's you")
+    import time
+    time.sleep(300)
+    print("it's me!")
 
 
 @pipeline(enable_cache=False, settings={"docker": docker_settings})
